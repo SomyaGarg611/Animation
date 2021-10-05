fb0c07c7eafd8ef0125e03d2b5fbbd83373c5b69
SomyaGarg <somyagarg270@gmail.com>
2021-10-05 22:31:45 +0530

    Animated file

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var id = null;
+var pos=0;
 function myMove() {
   var elem = document.getElementById("sail");
-  var pos = 0;
   clearInterval(id);
   id = setInterval(frame, 10);
   function frame() {
-    if (pos == 1190) {
-      clearInterval(id);
-    } else {
+  if (pos == 1160)   {
+      clearInterval(id);}
+    else {
       pos++;
-      
+
       elem.style.left = pos + 'px';
     }
   }
+}
+function Stop() {
+  var elem = document.getElementById("sail");
+  clearInterval(id);
 }
