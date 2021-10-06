ab0de167058b5c14259f1595380fabe1f372dac9
SomyaGarg <somyagarg270@gmail.com>
2021-10-06 10:42:38 +0530

    Final

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 var id = null;
-var pos=0;
+var pos = 0;
 function myMove() {
   var elem = document.getElementById("sail");
   clearInterval(id);
-  id = setInterval(frame, 10);
+  id = setInterval(frame, 20);
   function frame() {
-  if (pos == 1160)   {
-      clearInterval(id);}
+    if (pos == 1160) {
+      pos=0;
+      elem.style.left = pos + 'px';
+      
+    }
     else {
       pos++;
-
       elem.style.left = pos + 'px';
+      
     }
   }
 }
